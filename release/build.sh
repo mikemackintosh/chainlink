db78ce17515b33bb6161f104b637d613e85f9524
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2023-03-10 22:54:43 -0500

    Update build.sh

--- release/build.sh
+++ release/build.sh
@@ -34,7 +34,7 @@ fi
 for os in ${OS[@]}; do 
     for arch in ${ARCH[@]}; do
         echo -e "\nBuilding for $os-$arch"
-        GOOS=$os GOARCH=$arch go build -ldflags="-s -w ${LDFLAGS[*]}" -o release/bin/ninetails-$os-$arch cmd/main.go
-        echo $(shasum -a 256 release/bin/ninetails-$os-$arch)
+        GOOS=$os GOARCH=$arch go build -ldflags="-s -w ${LDFLAGS[*]}" -o release/bin/chainlink-$os-$arch cmd/main.go
+        echo $(shasum -a 256 release/bin/chainlink-$os-$arch)
     done
 done
